56b64b9a42de5a79212d0cbbcbac761ee5d3cdd4
unknown <b.e.m.schaub@uva.nl>
2019-11-22 12:39:45 +0100

    Ik wil wat informatie toevoegen

--- brt1.R
+++ brt1.R
@@ -1,3 +1,9 @@
 getwd()
 # my repo
 My first ever Github repository
+
+Exercise:
+Write a few lines.
+See what happ in the git tab
+Do the add + commit + to habe your local chages 
+also visible online.
